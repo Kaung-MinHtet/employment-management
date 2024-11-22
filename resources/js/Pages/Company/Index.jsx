8ca655cf4d0eef2c34c2ad7a268753364f173f06
kaungminhtet <kaungmihtet@agga.io>
2024-11-22 11:18:52 +0630

    Update delete: updated delete function

--- resources/js/Pages/Company/Index.jsx
+++ resources/js/Pages/Company/Index.jsx
@@ -139,7 +139,7 @@ const Index = ({ toast_message }) => {
             {showModal && (
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
                     <div className="bg-white p-6 rounded-md">
-                        <h3 className="text-lg font-semibold mb-4">Are you sure you want to delete this company?</h3>
+                        <h3 className="text-lg font-semibold mb-4">Are you sure you want to delete this company?<br/><span className="font-normal text-base text-center block text-gray-600">Employees in this company will also be deleted.</span></h3>
                         <div className="flex justify-end gap-4">
                             <button
                                 className="px-4 py-2 bg-red-500 text-white rounded"
